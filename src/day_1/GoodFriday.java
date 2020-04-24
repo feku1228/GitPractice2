8c2dcd168c90321a1f2d9c558c142129167597ca
Svitlana Fedorova <63323272+feku1228@users.noreply.github.com>
2020-04-24 18:10:18 -0400

    Update GoodFriday.java

--- src/day_1/GoodFriday.java
+++ src/day_1/GoodFriday.java
@@ -3,5 +3,6 @@ package day_1;
 public class GoodFriday {
     public static void main(String[] args) {
         System.out.println("Wonderful Day");
+        System.out.println("Awesome job");
     }
 }
