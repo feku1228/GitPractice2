c0551bb2e4598db4eb9890af9ad85917d1c39698
feku1228 <feku1228@gmail.com>
2020-04-24 16:15:58 -0400

    add Login.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -2,7 +2,8 @@ package day_1;
 
 public class AllGood {
     public static void main(String[] args) {
-        // I hope every thing is GOOD.
+        // I hope everything is GOOD.
+        System.out.println("All good");
 
     }
 }
